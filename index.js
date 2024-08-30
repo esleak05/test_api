6083471fe53170304d685caa6abac97f05125901
esleak <esleak@mespinoza.caep.corp>
2024-08-30 10:20:12 -0400

    initial Project

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+
+let people=  [
+    { id: 1, name: 'John Doe', age: 30},
+    { id: 1, name: 'Jane smith', age: 25},
+    { id: 1, name: 'Bob Johnson', age: 20},
+
+];
+
+app.get('/people', (req, res) => {
+    res.json(people)
+});
+
+
+app.get('/people/:id', (req, res) => {
+    const person = people.find(p => p.id == parseInt( req.params.id))
+    if (!person) {
+        return res.status(404).send('Person not found');
+    }
+
+    res.json(person);
+})
+
+
+
+app.post('people', (req, res) => {
+
+    const newPerson = {
+        id: people.length + 1,
+        name: req.body.name,
+        age: req.body.age
+    };
+
+    people.push(newPerson);
+    res.status(201).json(newPerson);
+
+})
+
+
+
+app.delete('people/:id', (req, res) => {
+    const personIdenx = people.findIndex(p => p.id === parseInt(req.params.id))
+    if(personIdenx === -1){
+        return res.status(404).send('Person not found');
+    }
+
+    const deletePerson  = people.splice(personIndex, 1);
+
+    res.json(deletePerson[0]);
+
+})
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+})
