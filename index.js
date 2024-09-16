b1a10802c4238015bdd640f676af8e55bb8ae596
esleak <esleak@mespinoza.caep.corp>
2024-09-16 09:11:38 -0300

    Se agrega lista blanca de dominios permitidos para uitilizar api

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const cors = require('cors')
 
 const PORT = 8080
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
+const whiteList = ["http://localhost:5173", "http://34.176.196.36"] 
+
+const corsOption = {
+    origin: function(origin, callback){
+        if (whiteList.indexOf(origin) !== -1 || !origin ) {
+            callback(null, true);
+        }
+        else{
+            callback(new Error('Not allowed by CORS'))
+        } 
+    }
+}
+app.use(cors(corsOption));
 
 
 let people=  [
